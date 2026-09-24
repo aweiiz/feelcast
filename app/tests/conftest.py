1e87b0433fe19f640a7e6cdc8a7085c25e8c2f28
aweiiz7 <aweiiz.7n@gmail.com>
2026-09-24 18:39:26 +0300

    fix: add DB

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,7 +1,31 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from app.main import app
+from app.db.database import get_db
+from app.db.models import Base
+
+# Тестовая БД в памяти
+engine = create_engine("sqlite:///:memory:")
+TestingSessionLocal = sessionmaker(bind=engine)
+
+
+@pytest.fixture(autouse=True)
+def setup_db():
+    Base.metadata.create_all(engine)
+    yield
+    Base.metadata.drop_all(engine)
+
 
 @pytest.fixture
 def client():
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
     return TestClient(app)
